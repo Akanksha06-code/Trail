e7abcfbb9b06ae5b1311bf06256499dfe5c72d77
Akanksha Kumari <akanksha260204@gmail.com>
2025-05-15 16:44:05 +0530

    second commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:8000",
+        origin: process.env.CORS_ORIGIN,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
 
