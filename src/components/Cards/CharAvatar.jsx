b29dc1c113a0290e707128eacd20cc53c539e485
vishakha <vishakhasingh423@gmail.com>
2025-05-17 14:19:04 +0530

    third commit

--- src/components/Cards/CharAvatar.jsx
+++ src/components/Cards/CharAvatar.jsx
@@ -4,7 +4,7 @@ const CharAvatar = ({fullName,width,height,style}) => {
     return(
         <div
             className= {`${width ||"w-12"} ${height ||"h-12"}  ${style || "text-2xl"}
-            flex items center justify-center rounded-full text-yellow-500 bg-gray-300 font-medium mt-6`}>
+            flex items center justify-center rounded-full text-yellow-500 bg-gray-100 font-medium mt-20`}>
             {getInitials(fullName || "")}
             </div>
     );
