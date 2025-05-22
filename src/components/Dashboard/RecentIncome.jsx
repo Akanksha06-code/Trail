9010ac1d11b7d1632085cec752a7db5be542dc5c
vishakha <vishakhasingh423@gmail.com>
2025-05-22 06:28:20 +0530

    third commit

--- src/components/Dashboard/RecentIncome.jsx
+++ src/components/Dashboard/RecentIncome.jsx
@@ -20,7 +20,7 @@ const RecentIncome = ({transactions,onSeeMore}) =>{
             title={item.source}
             icon={item.icon}
             date={moment(item.date).format("DD MMM YYYY")}
-            amount={income.amount}
+
             type="income"
             hideDeleteBtn
             />
