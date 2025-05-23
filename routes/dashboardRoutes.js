2a540985c6cbf8d4b6df723645b3579682290992
Akanksha <akanksha260204@gmail.com>
2025-05-23 08:21:06 +0530

    Delete

--- routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getDashboardData } = require('../controllers/dashboardController');
-const { Protect } = require('../middleware/authmiddleware');
-
-const router = express.Router();
-
-router.get('/', Protect, getDashboardData);
-
-module.exports = router;
